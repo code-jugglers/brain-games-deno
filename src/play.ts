543393fa6b8eea17562ecf93921ef8ba75cdc2ab
dblue <dblue@deloitte.com>
2020-06-01 09:10:37 -0400

    prompt user for desired player

--- src/play.ts
+++ src/play.ts
@@ -1,16 +1,20 @@
-import { resolve } from "https://deno.land/std@0.51.0/path/mod.ts";
-
 import { Board, BoardSpace } from "./board.ts";
 import { Bot } from "./bot.ts";
 
 export class PlayProgram {
-  private bot: Bot = new Bot(this.board, BoardSpace.O, "team_o_brain.json");
+  private bot: Bot = new Bot(
+    this.board,
+    BoardSpace.O,
+    `team_${this.bot_team}_brain.json`.toLowerCase()
+  );
 
-  constructor(public board: Board) {
-    this.play(BoardSpace.X);
-  }
+  constructor(
+    public board: Board,
+    public player_team: BoardSpace,
+    public bot_team: BoardSpace
+  ) {}
 
-  async play(currentPlayer: BoardSpace) {
+  async play(current_player: BoardSpace) {
     console.log(this.board.createVisual());
     console.log(" ");
 
@@ -32,19 +36,21 @@ export class PlayProgram {
       return;
     }
 
-    if (currentPlayer === BoardSpace.X) {
+    if (current_player === this.player_team) {
       const move = await this.getUserMove();
 
-      this.board.set(move[1], move[0], BoardSpace.X);
+      this.board.set(move[1], move[0], this.player_team);
     } else {
       const move = this.bot.determineMove();
 
       if (move) {
-        this.board.setByIndex(move.index, BoardSpace.O);
+        this.board.setByIndex(move.index, this.bot_team);
       }
     }
 
-    this.play(currentPlayer === BoardSpace.X ? BoardSpace.O : BoardSpace.X);
+    this.play(
+      current_player === this.player_team ? this.bot_team : this.player_team
+    );
   }
 
   async getUserMove(): Promise<number[]> {
@@ -60,4 +66,27 @@ export class PlayProgram {
   }
 }
 
-new PlayProgram(new Board());
+await Deno.stdout.write(
+  new TextEncoder().encode("Would you like to be X or O? (X): ")
+);
+
+const buf = new Uint8Array(1024);
+const n = <number>await Deno.stdin.read(buf);
+const answer = new TextDecoder()
+  .decode(buf.subarray(0, n))
+  .toUpperCase()
+  .trim();
+
+let program: PlayProgram;
+
+if (answer === "O") {
+  console.log(`You will be O. Good Luck.`);
+
+  program = new PlayProgram(new Board(), BoardSpace.O, BoardSpace.X);
+} else {
+  console.log(`You will be X. Good Luck.`);
+
+  program = new PlayProgram(new Board(), BoardSpace.X, BoardSpace.O);
+}
+
+program.play(BoardSpace.X);
